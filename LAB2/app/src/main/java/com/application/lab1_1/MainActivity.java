8ceb1f6b05af81c61318d454ada3db36430c475b
Mayur Bhoi <mayur072000@gmail.com>
2021-03-02 02:54:45 +0530

    Fixes for Q1, Q2 of Lab 2

--- LAB2/app/src/main/java/com/application/lab1_1/MainActivity.java
+++ LAB2/app/src/main/java/com/application/lab1_1/MainActivity.java
@@ -17,197 +17,117 @@ public class MainActivity extends AppCompatActivity {
     float operand1, operand2;
     boolean add, sub, mul, div;
 
+
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        b0 = (Button) findViewById(R.id.button0);
-        b1 = (Button) findViewById(R.id.button1);
-        b2 = (Button) findViewById(R.id.button2);
-        b3 = (Button) findViewById(R.id.button3);
-        b4 = (Button) findViewById(R.id.button4);
-        b5 = (Button) findViewById(R.id.button5);
-        b6 = (Button) findViewById(R.id.button6);
-        b7 = (Button) findViewById(R.id.button7);
-        b8 = (Button) findViewById(R.id.button8);
-        b9 = (Button) findViewById(R.id.button9);
-        bClr = (Button) findViewById(R.id.buttonC);
-        bAdd = (Button) findViewById(R.id.buttonadd);
-        bSub = (Button) findViewById(R.id.buttonsub);
-        bMul = (Button) findViewById(R.id.buttonmul);
-        bDiv = (Button) findViewById(R.id.buttondiv);
-        bEql = (Button) findViewById(R.id.buttoneql);
-        editText1 = (EditText) findViewById(R.id.editText1);
+        b0 = findViewById(R.id.button0);
+        b1 = findViewById(R.id.button1);
+        b2 = findViewById(R.id.button2);
+        b3 = findViewById(R.id.button3);
+        b4 = findViewById(R.id.button4);
+        b5 = findViewById(R.id.button5);
+        b6 = findViewById(R.id.button6);
+        b7 = findViewById(R.id.button7);
+        b8 = findViewById(R.id.button8);
+        b9 = findViewById(R.id.button9);
+        bClr = findViewById(R.id.buttonC);
+        bAdd = findViewById(R.id.buttonadd);
+        bSub = findViewById(R.id.buttonsub);
+        bMul = findViewById(R.id.buttonmul);
+        bDiv = findViewById(R.id.buttondiv);
+        bEql = findViewById(R.id.buttoneql);
+        editText1 = findViewById(R.id.editText1);
 
-        b0.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                editText1.setText(editText1.getText() + "0");
-            }
-        });
+        b0.setOnClickListener(v -> editText1.setText(editText1.getText() + "0"));
+        b1.setOnClickListener(v -> editText1.setText(editText1.getText() + "1"));
+        b2.setOnClickListener(v -> editText1.setText(editText1.getText() + "2"));
+        b3.setOnClickListener(v -> editText1.setText(editText1.getText() + "3"));
+        b4.setOnClickListener(v -> editText1.setText(editText1.getText() + "4"));
+        b5.setOnClickListener(v -> editText1.setText(editText1.getText() + "5"));
+        b6.setOnClickListener(v -> editText1.setText(editText1.getText() + "6"));
+        b7.setOnClickListener(v -> editText1.setText(editText1.getText() + "7"));
 
-        b1.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                editText1.setText(editText1.getText() + "1");
-            }
-        });
-        b2.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                editText1.setText(editText1.getText() + "2");
-            }
-        });
-        b3.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                editText1.setText(editText1.getText() + "3");
-            }
-        });
-        b4.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                editText1.setText(editText1.getText() + "4");
-            }
-        });
-        b5.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                editText1.setText(editText1.getText() + "5");
-            }
-        });
-        b6.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                editText1.setText(editText1.getText() + "6");
-            }
-        });
-        b7.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                editText1.setText(editText1.getText() + "7");
-            }
-        });
+        b8.setOnClickListener(v -> editText1.setText(editText1.getText() + "8"));
 
-        b8.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                editText1.setText(editText1.getText() + "8");
-            }
-        });
+        b9.setOnClickListener(v -> editText1.setText(editText1.getText() + "9"));
 
-        b9.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                editText1.setText(editText1.getText() + "9");
-            }
-        });
+        bAdd.setOnClickListener(v -> {
 
-        bAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (editText1 == null) {
-                    editText1.setText("");
-                } else {
-                    operand1 = Float.parseFloat(editText1.getText() + "");
-                    add = true;
-                    editText1.setText(null);
-                }
-            }
-        });
-
-        bSub.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (editText1 == null) {
-                    editText1.setText("");
-                } else {
-                    operand1 = Float.parseFloat(editText1.getText() + "");
-                    sub = true;
-                    editText1.setText(null);
-                }
-            }
-        });
-
-        bMul.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (editText1 == null) {
-                    editText1.setText("");
-                } else {
-                    operand1 = Float.parseFloat(editText1.getText() + "");
-                    mul = true;
-                    editText1.setText(null);
-                }
-            }
-        });
-
-        bDiv.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                if (editText1 == null) {
-                    editText1.setText("");
-                } else {
-                    operand1 = Float.parseFloat(editText1.getText() + "");
-                    div = true;
-                    editText1.setText(null);
-                }
-            }
-        });
-
-        bClr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+            if (editText1 == null) {
                 editText1.setText("");
+            } else {
+                operand1 = Float.parseFloat(editText1.getText() + "");
+                add = true;
+                editText1.setText(null);
             }
         });
 
+        bSub.setOnClickListener(v -> {
 
-        bEql.setOnClickListener(new View.OnClickListener() {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void onClick(View v) {
-                operand2 = Float.parseFloat(editText1.getText() + "");
-
-                if (add) {
-                    result = operand1 + operand2 + "";
-                    add = false;
-                }
-
-                if (sub) {
-                    result = operand1 - operand2 + "";
-                    sub = false;
-                }
-
-                if (mul) {
-                    result = operand1 * operand2 + "";
-                    mul = false;
-                }
-
-                if (div) {
-                    result = operand1 / operand2 + "";
-                    div = false;
-                }
-
-                Intent putResultIntent = new Intent(MainActivity.this, ResultActivity.class);
-                putResultIntent.putExtra("result", result.toString());
-                MainActivity.this.startActivity(putResultIntent);
+            if (editText1 == null) {
+                editText1.setText("");
+            } else {
+                operand1 = Float.parseFloat(editText1.getText() + "");
+                sub = true;
+                editText1.setText(null);
             }
+        });
+
+        bMul.setOnClickListener(v -> {
+
+            if (editText1 == null) {
+                editText1.setText("");
+            } else {
+                operand1 = Float.parseFloat(editText1.getText() + "");
+                mul = true;
+                editText1.setText(null);
+            }
+        });
+
+        bDiv.setOnClickListener(v -> {
+
+            if (editText1 == null) {
+                editText1.setText("");
+            } else {
+                operand1 = Float.parseFloat(editText1.getText() + "");
+                div = true;
+                editText1.setText(null);
+            }
+        });
+
+        bClr.setOnClickListener(v -> editText1.setText(""));
+
+
+        bEql.setOnClickListener(v -> {
+            operand2 = Float.parseFloat(editText1.getText() + "");
+
+            if (add) {
+                result = operand1 + operand2 + "";
+                add = false;
+            }
+
+            if (sub) {
+                result = operand1 - operand2 + "";
+                sub = false;
+            }
+
+            if (mul) {
+                result = operand1 * operand2 + "";
+                mul = false;
+            }
+
+            if (div) {
+                result = operand1 / operand2 + "";
+                div = false;
+            }
+
+            Intent putResultIntent = new Intent(MainActivity.this, ResultActivity.class);
+            putResultIntent.putExtra("result", result);
+            MainActivity.this.startActivity(putResultIntent);
         });
     }
 
